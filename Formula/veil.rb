ca442ad71f0e4fe59dd85c0427d60dc6cf25ebc0
Ushira Dineth <ushiradineth@gmail.com>
2026-03-13 17:56:53 +0530

    refactor(tap): switch veil from cask to formula
    Signed-off-by: Ushira Dineth <ushiradineth@gmail.com>

--- Formula/veil.rb
+++ Formula/veil.rb
@@ -1,15 +1,19 @@
 # This file is managed by veil release automation. DO NOT EDIT.
-cask "veil" do
-  name "veil"
+class Veil < Formula
   desc "Fast CLI and skill for local code retrieval and agent context workflows"
   homepage "https://github.com/ushiradineth/veil"
-  version "0.2.1"
   url "https://registry.npmjs.org/@ushiradineth/veil/-/veil-0.2.1.tgz"
   sha256 "c846b87a2178d560b346c1344b179f734ede2b737b5cbe460bfe10381951d006"
+  version "0.2.1"
+  license "MIT"
 
-  depends_on formula: "node"
+  depends_on "node"
 
-  binary "package/bin/veil.mjs", target: "veil"
+  def install
+    system "npm", "install", "-g", "--prefix", prefix, "."
+  end
 
-  # No zap stanza required
+  test do
+    assert_match "Usage:", shell_output("#{bin}/veil 2>&1", 1)
+  end
 end
